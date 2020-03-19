588d619c079d6a3bf9c12fa78364d382f728b410
swathi <mail@gmail.com>
2020-03-19 15:00:14 +0530

    Jenkins-Ant

---
+++ src/Hello1.java
@@ -0,0 +1,6 @@
+public class Hello1 {
+        public static void main(String[] args) {
+                // Prints "Hello1" in the terminal window.
+                System.out.println("Hello1");
+        }
+}
---
+++ src/Hello4.java
@@ -0,0 +1,6 @@
+public class Hello4 {
+        public static void main(String[] args) {
+                // Prints "Hello1" in the terminal window.
+                System.out.println("Hello4");
+        }
+}
